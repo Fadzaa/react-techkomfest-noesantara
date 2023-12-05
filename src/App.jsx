4657b0ab34ae3b27e8fc90a0546f42b7dd402242
Nozell77 <rafisidqi999@gmail.com>
2023-12-05 15:08:13 +0700

    fix: [RS] fix footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import LandingPage from './pages/LandingPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './global_component/Navbar';
-import LandingComponentIndonesia from './component/LandingComponent/LandingComponentIndonesia';
+import LandingComponentTestimony from './component/LandingComponent/LandingComponentTestimony';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/indonesia" element={<LandingComponentIndonesia />} />
+        <Route path="/testimony" element={<LandingComponentTestimony />} />
       </Routes>
     </BrowserRouter>
   );
