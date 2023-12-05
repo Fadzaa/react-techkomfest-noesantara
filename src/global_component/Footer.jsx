7c86841cf5198bff938e41beb0a62c8deeda7787
Nozell77 <rafisidqi999@gmail.com>
2023-12-05 12:12:38 +0700

    feat: [RS] add footer

--- src/global_component/Footer.jsx
+++ src/global_component/Footer.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from 'react';
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div className="bg-primary pt-[120px] px-32 items-center h-screen  flex flex-col gap-56 ">
+        <div className="flex gap-40">
+          <div>
+            <h2 className="font-urbanist font-semibold text-white text-2xl">Contact</h2>
+            <div className="text-white text-base font-urbanist">
+              <p>0895395343223</p>
+              <p>fattahganteng@gmail.com</p>
+            </div>
+          </div>
+          <div>
+            <h2 className="font-urbanist font-semibold text-white text-2xl">Shop</h2>
+            <div className="text-white text-base font-urbanist">
+              <p>Tops</p>
+              <p>Bottoms</p>
+              <p>Outerwear</p>
+              <p>New in</p>
+              <p>About</p>
+              <p>Blog</p>
+              <p>Contact</p>
+            </div>
+          </div>
+          <div>
+            <h2 className="font-urbanist font-semibold text-white text-2xl">Company</h2>
+            <div className="text-white text-base font-urbanist">
+              <p>Cookies</p>
+              <p>Payments</p>
+              <p>Terms & Conditions</p>
+              <p>Privacy Policy</p>
+              <p>Security</p>
+            </div>
+          </div>
+          <div>
+            <h2 className="font-urbanist font-semibold text-white text-2xl">Newsletter</h2>
+            <div className="text-white text-base font-urbanist">
+              <p>Be the first to hear about our latest offers</p>
+              <div className="flex items-center">
+                <input type="text" placeholder="YOUR EMAIL ADDRESS" pattern="[a-zA-Z0-9._%+-]+@gmail\.com" className="w-full h-10 px-3 mt-3 border border-white bg-transparent rounded-sm focus:outline-none placeholder-white" />
+                <a href="#">
+                  <button className="border border-white rounded-sm h-10 w-10 mt-3">
+                    <ion-icon name="send-sharp"></ion-icon>
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <hr className="border-1 border-white w-screen" />
+          </div>
+          <div className="flex mt-7 justify-center  space-x-96">
+            <div>
+               <p className="text-white font-urbanist text-base">Copyright &copy; 2023 Lorem Ipsum Ltd. </p>
+            </div>
+            <div className="flex gap-4 text-white ">
+              <ion-icon name="logo-instagram" size='large'></ion-icon>
+              <ion-icon name="logo-facebook" size='large'></ion-icon>
+              <ion-icon name="logo-twitter" size='large'></ion-icon>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
