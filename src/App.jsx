8b9e8ef7dd800d16cf4f0abb16767e8e77d1642f
Nozell77 <rafisidqi999@gmail.com>
2023-12-07 05:01:22 +0700

    feat: [RS] add GalleryComponentHero

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import LandingPage from './pages/LandingPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingComponentTestimony from './component/LandingComponent/LandingComponentTestimony';
+import GalleryComponentHero from './component/GalleryComponent/GalleryComponentHero';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/testimony" element={<LandingComponentTestimony />} />
+        <Route path="/gallery" element={<GalleryComponentHero />} />
       </Routes>
     </BrowserRouter>
   );
--- src/utils/images.js
+++ src/utils/images.js
@@ -4,6 +4,13 @@ import rainbowShape from '../assets/image/rainbow_shape.png'
 import indonesia1 from '../assets/image/indonesia1.png'
 import indonesia2 from '../assets/image/indonesia2.png'
 import people from '../assets/image/people.png'
+import bgGallery1 from '../assets/image/gallery1.png'
+import bgGallery2 from '../assets/image/gallery2.png'
+import bgGallery3 from '../assets/image/gallery3.png'
+import bgGallery4 from '../assets/image/gallery4.png'
+import bgGallery5 from '../assets/image/gallery5.png'
+import bgGallery6 from '../assets/image/gallery6.png'
+import bgGallery7 from '../assets/image/gallery7.png'
 
 //Example
 export { 
@@ -13,5 +20,12 @@ export {
     indonesia1,
     indonesia2,
     people,
+    bgGallery1,
+    bgGallery2,
+    bgGallery3, 
+    bgGallery4,
+    bgGallery5,
+    bgGallery6,
+    bgGallery7,
 
 }
