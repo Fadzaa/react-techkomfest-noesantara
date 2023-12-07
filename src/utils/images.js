039fe960f54e15a7c60a687ee322afc5aa89eb8c
Fadzaa <fadza20@gmail.com>
2023-12-07 20:15:56 +0700

    resolve

--- src/utils/images.js
+++ src/utils/images.js
@@ -4,9 +4,7 @@ import rainbowShape from '../assets/image/rainbow_shape.png'
 import indonesia1 from '../assets/image/indonesia1.png'
 import indonesia2 from '../assets/image/indonesia2.png'
 import people from '../assets/image/people.png'
-<<<<<<< HEAD
 import bgInformative1 from '../assets/image/bg_informative_1.png'
-=======
 import bgGallery1 from '../assets/image/gallery1.png'
 import bgGallery2 from '../assets/image/gallery2.png'
 import bgGallery3 from '../assets/image/gallery3.png'
@@ -14,7 +12,6 @@ import bgGallery4 from '../assets/image/gallery4.png'
 import bgGallery5 from '../assets/image/gallery5.png'
 import bgGallery6 from '../assets/image/gallery6.png'
 import bgGallery7 from '../assets/image/gallery7.png'
->>>>>>> b430394743d21d494f2301a8c761079ea37801ae
 
 //Example
 export { 
@@ -24,9 +21,7 @@ export {
     indonesia1,
     indonesia2,
     people,
-<<<<<<< HEAD
-    bgInformative1
-=======
+    bgInformative1,
     bgGallery1,
     bgGallery2,
     bgGallery3, 
@@ -34,6 +29,5 @@ export {
     bgGallery5,
     bgGallery6,
     bgGallery7,
->>>>>>> b430394743d21d494f2301a8c761079ea37801ae
 
 }
