b362237be326f14f983d135688a64f45f2ed6956
Nozell77 <rafisidqi999@gmail.com>
2023-12-10 17:27:34 +0700

    feat: [RS] add responsive QuizComponent and AnswerComponent

--- src/component/QuizComponent/AnswerComponent.jsx
+++ src/component/QuizComponent/AnswerComponent.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const AnswerComponent = () => {
   return (
     <>
-    <div className='flex justify-center items-center mt-9'>
-         <div className='grid grid-cols-2 gap-7 text-white font-urbanist font-semibold text-2xl  '>
-        <div><a href="#"><button className='border border-white w-[400px] h-[67px] rounded-xl hover:bg-[#EAF2E2] hover:text-primary hover:border-primary'>A</button></a></div>
-        <div><a href="#"><button className='border border-white w-[400px] h-[67px] rounded-xl hover:bg-[#EAF2E2] hover:text-primary hover:border-primary'>B</button></a></div>
-        <div><a href="#"><button className='border border-white w-[400px] h-[67px] rounded-xl hover:bg-[#EAF2E2] hover:text-primary hover:border-primary'>C</button></a></div>
-        <div><a href="#"><button className='border border-white w-[400px] h-[67px] rounded-xl hover:bg-[#EAF2E2] hover:text-primary hover:border-primary'>D</button></a></div>
+    <div className='flex justify-center items-center lg:mt-9'>
+         <div className=' text-white font-urbanist font-semibold text-2xl flex flex-col gap-[19px] lg:grid lg:grid-cols-2 lg:gap-7  '>
+        <div><a href="#"><button className='border border-white w-[300px] h-[67px] lg:w-[400px] lg:h-[67px] rounded-xl lg:hover:bg-[#EAF2E2] lg:hover:text-primary lg:hover:border-primary'>A</button></a></div>
+        <div><a href="#"><button className='border border-white w-[300px] h-[67px] lg:w-[400px] lg:h-[67px] rounded-xl lg:hover:bg-[#EAF2E2] lg:hover:text-primary lg:hover:border-primary'>B</button></a></div>
+        <div><a href="#"><button className='border border-white w-[300px] h-[67px] lg:w-[400px] lg:h-[67px] rounded-xl lg:hover:bg-[#EAF2E2] lg:hover:text-primary lg:hover:border-primary'>C</button></a></div>
+        <div><a href="#"><button className='border border-white w-[300px] h-[67px] lg:w-[400px] lg:h-[67px] rounded-xl lg:hover:bg-[#EAF2E2] lg:hover:text-primary lg:hover:border-primary'>D</button></a></div>
     </div>
     </div>
    
@@ -16,4 +16,4 @@ const AnswerComponent = () => {
   )
 }
 
-export default AnswerComponent
+export default AnswerComponent 
