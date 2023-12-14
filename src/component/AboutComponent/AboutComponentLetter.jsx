85e67723ac0b0e84df9c5cdc5fa075701da2960d
Nozell77 <rafisidqi999@gmail.com>
2023-12-14 13:24:19 +0700

    feat: [RS] add AboutComponenetLetter and responsive

--- src/component/AboutComponent/AboutComponentLetter.jsx
+++ src/component/AboutComponent/AboutComponentLetter.jsx
@@ -1,4 +1,4 @@
-import React,{ useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { letterDecoration, letterDecorationmobile } from '../../utils/images';
 
 const AboutComponentLetter = () => {
