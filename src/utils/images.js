6ef44bd9c14689e7e0b33010662c8d8811522798
Nozell77 <rafisidqi999@gmail.com>
2023-12-18 12:13:49 +0700

    feat: [RS] add Responsive HouseDetailTraditional

--- src/utils/images.js
+++ src/utils/images.js
@@ -39,6 +39,7 @@ import foodGudeg from "../assets/image/food_gudeg.png";
 import foodRendang from "../assets/image/food_rendang.png";
 import foodSatai from "../assets/image/food_satai.png";
 import foodTempe from "../assets/image/food_tempe.png";
+import cardHouse from "../assets/image/cardTraditionalHouse.svg";
 
 
 
@@ -84,5 +85,6 @@ export {
     foodGudeg,
     foodRendang,
     foodSatai,
-    foodTempe
+    foodTempe,
+    cardHouse
 }
