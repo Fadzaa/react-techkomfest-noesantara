453f40686cc26df3c9646a49f0123b1dd5a7b4f5
Nozell77 <rafisidqi999@gmail.com>
2023-12-21 01:28:22 +0700

    feat: [RS] logic gallery

---
+++ src/component/GalleryComponent/dataGallery.js
@@ -0,0 +1,75 @@
+const dataGallery = [
+    {
+        "id": 1,
+        "culture_name": "Rendang",
+        "category": "Makanan Tradisional",
+        "province": "Sumatera",
+        "culture_image": "https://www.astronauts.id/blog/wp-content/uploads/2023/03/Resep-Rendang-Daging-Sapi-Untuk-Lebaran-Gurih-dan-Nikmat-1024x683.jpg"
+    },
+    {
+        "id": 2,
+        "culture_name": "Batik",
+        "category": "Warisan Kebudayaan",
+        "province": "Surakarta",
+        "culture_image": "https://images.unsplash.com/photo-1525845859779-54d477ff291f?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        "id": 3,
+        "culture_name": "Wayang",
+        "category": "Warisan Kebudayaan",
+        "province": "Jawa",
+        "culture_image": "https://images.unsplash.com/photo-1620409123739-3849e65df57a?q=80&w=3027&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        "id": 4,
+        "culture_name": "Joglo",
+        "category": "Rumah Adat",
+        "province": "Jawa",
+        "culture_image": "https://i0.wp.com/www.emporioarchitect.com/upload/portofolio/desain-rumah-tradisional-1-lantai-12220822-78764878220822092641-0.jpg"
+    },
+    {
+        "id": 5,
+        "culture_name": "Tari Saman",
+        "category": "Tari Tradisional",
+        "province": "Aceh",
+        "culture_image": "https://www.goodnewsfromindonesia.id/uploads/post/large-fifa-sqi-indonesia-tari-saman-80dbc564301e461d21980270fe09aa07.jpg"
+    },
+    {
+        "id": 6,
+        "culture_name": "Angklung",
+        "category": "Alat Musik Tradisional",
+        "province": "Jawa",
+        "culture_image": "https://www.blibli.com/friends-backend/wp-content/uploads/2021/04/Angklung-1.jpg"
+    },
+    {
+        "id": 7,
+        "culture_name": "Keris",
+        "category": "Senjata Tradisional",
+        "province": "Jawa",
+        "culture_image": "https://sejarahbudaya.com/wp-content/uploads/2023/03/Keris-Naga-Siluman-Luk-13-Pamor-Wos-Wutah5.jpg"
+    },
+    {
+        "id": 8,
+        "culture_name": "Gamelan",
+        "category": "Alat Musik Tradisional",
+        "province": "Jawa",
+        "culture_image": "https://upload.wikimedia.org/wikipedia/commons/2/26/Traditional_indonesian_instruments02.jpg"
+    },
+    {
+        "id": 9,
+        "culture_name": "Tari Kecak",
+        "category": "Tari Tradisional",
+        "province": "Bali",
+        "culture_image": "https://awsimages.detik.net.id/community/media/visual/2020/08/25/tari-kecak-1_169.jpeg?w=1200"
+    },
+    {
+        "id": 10,
+        "culture_name": "Rumah Gadang",
+        "category": "Rumah Adat",
+        "province": "Sumatera Barat",
+        "culture_image": "https://awsimages.detik.net.id/community/media/visual/2023/08/09/rumah-adat-sumatera-barat_169.jpeg?w=1200"
+    }
+]
+
+
+  export default dataGallery;
