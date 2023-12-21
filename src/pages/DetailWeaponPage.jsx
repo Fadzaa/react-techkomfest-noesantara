5af5916246b5a4d10f4915e06a57afb24716344c
Fadzaa <fadza20@gmail.com>
2023-12-21 12:12:09 +0700

    feat: [FA] add Colection Weapon Component

--- src/pages/DetailWeaponPage.jsx
+++ src/pages/DetailWeaponPage.jsx
@@ -4,12 +4,15 @@ import {bgHeroWeapon} from "../utils/images.js";
 import TraditionalWeaponComponent
     from "../component/DetailComponent/DetailWeaponComponent/TraditionalWeaponComponent.jsx";
 import RecordWeaponComponent from "../component/DetailComponent/DetailWeaponComponent/RecordWeaponComponent.jsx";
+import CollectionWeaponComponent
+    from "../component/DetailComponent/DetailWeaponComponent/CollectionWeaponComponent.jsx";
 
 function DetailWeaponPage() {
     return (
         <>
             <DetailComponentHero image={bgHeroWeapon} title="Traditional Weapon" subtitle={"DETAIL BUDAYA"} description={"menyimpan kejayaan di setiap mata pisau dan gagangnya."}/>
             <TraditionalWeaponComponent/>
+            <CollectionWeaponComponent/>
             <RecordWeaponComponent/>
         </>
     );
--- src/utils/icon.js
+++ src/utils/icon.js
@@ -6,7 +6,10 @@ import iconInstagram from "../assets/icon/icon_instagram.svg";
 import iconArrowDown from "../assets/icon/icon_arrow_down.svg";
 import iconThropy from "../assets/icon/icon_thropy.svg";
 import rainbowShape from "../assets/icon/rainbow_shape.svg";
-
+import wavesTop from "../assets/icon/waves_top.svg";
+import wavesDown from "../assets/icon/waves_down.svg";
+import rectangleBottomDecoration from "../assets/icon/rectangle_bottom_decoration.svg";
+import rectangleTopDecoration from "../assets/icon/rectangle_top_decoration.svg";
 
 export {
     logo,
@@ -16,5 +19,10 @@ export {
     iconInstagram,
     iconArrowDown,
     iconThropy,
-    rainbowShape
+    rainbowShape,
+    wavesTop,
+    wavesDown,
+    rectangleBottomDecoration,
+    rectangleTopDecoration
+
 }
