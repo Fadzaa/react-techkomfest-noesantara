dd3c270c6370bb00949f5477b089815c269fb305
Fadzaa <fadza20@gmail.com>
2023-12-22 05:37:34 +0700

    feat: [FA] fix grid and add search filter

--- src/component/MainComponent/GalleryComponent/GalleryComponentHero.jsx
+++ src/component/MainComponent/GalleryComponent/GalleryComponentHero.jsx
@@ -6,6 +6,11 @@ const GalleryComponentHero = () => {
   const [backgroundImages, setBackgroundImages] = useState([bgGallery5, bgGallery2, bgGallery6, bgGallery4]);
   const [currentBackgroundIndex, setCurrentBackgroundIndex] = useState(0);
   const [fade, setFade] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -25,33 +30,48 @@ const GalleryComponentHero = () => {
   
 
   return (
-    <>
-      <div className="w-screen h-screen relative bg-black  opacity-95">
-        <div className="relative w-screen h-screen items-center">
-          <img src={backgroundImages[currentBackgroundIndex]} alt="bggallery" className={`w-full h-full object-cover transition-opacity duration-500 ease-in-out ${fade ? 'opacity-0' : 'opacity-100'}`} />
-          <div className="absolute top-0 left-0 w-full h-full transition  ease-in-out"></div>
-          <div className="flex flex-col justify-center items-center lg:flex">
-            <div className="absolute top-[137px] lg:top-[290px] lg:left-[103px]">
-              <div className="flex justify-center items-center lg:items-center gap-5">
-                <div className="h-[1px] w-[50px] lg:h-[1px] lg:w-8 bg-white"></div>
-                <h3 className="font-yiBaiti mx-3 lg:text-[20px] text-white lg:mx-3 tracking-widest">GALLERY INDONESIA</h3>
-                <div className="h-[1px] w-[50px] lg:h-[1px] lg:w-8 bg-white"></div>
-              </div>
-              <h1 className="font-milonga mt-4 ml-[30px] text-[55px] lg:text-[100px] text-white lg:mb-8">Gallery</h1>
-              <p className="w-[300px] lg:mb-6 lg:mt-3 mt-3 ml-[30px] text-white"> Tiap gambar adalah pintu ajaib ke budaya dan keindahan yang tak terlupakan. </p>
-              <div className="border border-white mt-[55px] ml-[30px] w-[300px] h-[39px] rounded-[20px] flex lg:w-[511px] lg:h-[48px] lg:rounded-3xl lg:flex items-center gap-1 lg:gap-5 lg:text-sm">
-                <ion-icon name="search-outline" style={{ fontSize: '24px', margin: '0 10px', marginLeft: '20px', color: 'white' }}></ion-icon>
-                <input type="text" placeholder="What do you want to see today?" className="bg-transparent text-white w-full text-[10px] lg:text-base lg:h-[48px] rounded-3xl focus:outline-none" />
-              </div>
+      <div className="w-screen h-screen relative">
+        <div className="absolute w-full h-full bg-black opacity-95"></div>
+        <img src={backgroundImages[currentBackgroundIndex]} alt="bggallery"
+             className={`w-full h-full object-cover absolute transition-opacity duration-500 ease-in-out ${fade ? 'opacity-0' : 'opacity-100'}`}/>
+
+        <div className="absolute flex lg:flex lg:w-full lg:h-full lg:ps-20 lg:pe-5 ">
+
+          <div className="flex flex-col  lg:h-full lg:w-1/2 lg:justify-center lg:pe-5">
+
+            <div className="flex lg:items-center gap-5">
+              <div className="h-[1px] w-[50px] lg:h-[1px] lg:w-8 bg-white"></div>
+              <h3 className="font-yiBaiti mx-3 lg:text-[20px] text-white lg:mx-3 tracking-widest">GALLERY INDONESIA</h3>
+              <div className="h-[1px] w-[50px] lg:h-[1px] lg:w-8 bg-white"></div>
             </div>
-            <div className="lg:flex-row absolute lg:top-44 lg:right-16 top-[409px] mt-[28px]">
-              <GalleryTabBar />
+
+            <h1 className="font-milonga mt-4  text-[55px] lg:text-[100px] text-white lg:mb-8">Gallery</h1>
+            {/*INI*/}
+            <p className="lg:mb-6 lg:mt-3 mt-3 font-urbanist font-medium text-white"> Tiap gambar adalah pintu ajaib ke budaya
+              dan keindahan yang tak terlupakan. </p>
+
+            <div className="border border-white border-opacity-70 mt-[55px] h-[39px] rounded-[20px] flex  lg:h-[48px] lg:rounded-3xl lg:flex items-center gap-1 lg:gap-5 lg:text-sm">
+              <ion-icon name="search-outline"
+                        style={{fontSize: '24px', margin: '0 10px', marginLeft: '20px', color: 'white'}}></ion-icon>
+              <input type="text"
+                     placeholder="What do you want to see today?"
+                     value={searchQuery}
+                     onChange={handleSearchChange}
+                     className="bg-transparent font-urbanist text-white w-full text-[10px] lg:text-base lg:h-[48px] rounded-3xl focus:outline-none"/>
             </div>
+            {/*INI*/}
           </div>
-          <p className="lg:text-white lg:font-urbanist lg:text-base lg:absolute lg:bottom-4 lg:text-center lg:left-0 lg:right-0 hidden lg:block">Copyright &copy; 2023 Lorem Ipsum Ltd.</p>
+
+          <div className="lg:flex lg:right-16 lg:h-full lg:w-1/2 lg:justify-center lg:items-start lg:pt-[20vh]">
+            <GalleryTabBar searchQuery={searchQuery} />
+          </div>
         </div>
+
+
+
+        <p className="lg:text-white lg:font-urbanist lg:text-base lg:absolute lg:bottom-4 lg:text-center lg:left-0 lg:right-0 hidden lg:block">Copyright &copy; 2023
+          Lorem Ipsum Ltd.</p>
       </div>
-    </>
   );
 };
 
