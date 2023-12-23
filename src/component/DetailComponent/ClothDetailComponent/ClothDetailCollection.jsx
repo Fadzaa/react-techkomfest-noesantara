b727af4599c2b6847551b71b16edfac29d00099e
Fadzaa <fadza20@gmail.com>
2023-12-24 06:35:37 +0700

    remove slick css

--- src/component/DetailComponent/ClothDetailComponent/ClothDetailCollection.jsx
+++ src/component/DetailComponent/ClothDetailComponent/ClothDetailCollection.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Slider from "react-slick";
-import "~slick-carousel/slick/slick.css";
-import "~slick-carousel/slick/slick-theme.css";
 import {foodGudeg, foodRendang, foodSatai, foodTempe} from "../../../utils/images.js";
 
 const ClothDetailCollection = () => {
