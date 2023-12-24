f533b34c199d9bcd7bff6ac9358a0c2ac15bd4de
Nozell77 <rafisidqi999@gmail.com>
2023-12-24 10:32:07 +0700

    fix minor

--- src/component/DetailComponent/ClothDetailComponent/ClothDetailCardSlider.jsx
+++ src/component/DetailComponent/ClothDetailComponent/ClothDetailCardSlider.jsx
@@ -1,141 +1,69 @@
-import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import {
-  clothCard1,
-  clothCard2,
-  clothCard3,
-  clothCard4,
-} from "../../utils/images";
-
-const ClothDetailCardSlider = () => {
+function App() {
   const settings = {
-    className: "center",
-    autoplay: true,
-    dots: false,
+    dots: true,
     infinite: true,
-    speed: 2000,
+    speed: 500,
     slidesToShow: 3,
-    centerMode : true,
-    slidesToScroll: 3,
-    arrows: true,
-    centerPadding: "100px",
-
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,  
-          centerMode : true,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 3,
-          centerMode : true,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          centerMode : true,
-          dots: false,
-        },
-      },
-    ],
+    slidesToScroll: 1,
   };
-
   return (
-    <div className="w-full lg:w-[1200px] md:w-screen sm:w-screen mx-auto   ">
-      <Slider {...settings}>
-        <div className="w-[300px] lg:w-[368px] px-2">
-          <div className="relative">
-            <img
-              src={clothCard1}
-              alt=""
-              className="object-cover h-[323px] w-[300px] lg:h-[544px] lg:w-[368px]"
-            />
-            <div className="absolute bottom-5 left-5 text-white">
-              <h1 className="font-milonga lg:text-2xl">Kebaya Gajah Mada</h1>
-              <h2 className="font-urbanist lg:text-xl">Bali, Indonesia</h2>
+    <div className="w-3/4 m-auto">
+      <div className="mt-20">
+        <Slider {...settings}>
+          {data.map((d) => (
+            <div
+              key={d.name}
+              className="bg-white h-[450px] text-black rounded-xl"
+            >
+              <div className="h-56 bg-indigo-500 flex justify-center items-center rounded-t-xl">
+                <img src={d.img} alt="" className="h-44 w-44 rounded-full" />
+              </div>
+
+              <div className="flex flex-col items-center justify-center gap-4 p-4">
+                <p className="text-xl font-semibold">{d.name}</p>
+                <p className="text-center">{d.review}</p>
+                <button className="bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl">
+                  Read More
+                </button>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="w-[300px] lg:w-[368px] px-2">
-          <div className="relative">
-            <img
-              src={clothCard1}
-              alt=""
-              className="object-cover h-[323px] w-[300px] lg:h-[544px] lg:w-[368px]"
-            />
-            <div className="absolute bottom-5 left-5 text-white">
-              <h1 className="font-milonga lg:text-2xl">Kebaya Gajah Mada</h1>
-              <h2 className="font-urbanist lg:text-xl">Bali, Indonesia</h2>
-            </div>
-          </div>
-        </div>
-        <div className="w-[300px] lg:w-[368px] px-2">
-          <div className="relative">
-            <img
-              src={clothCard1}
-              alt=""
-              className="object-cover h-[323px] w-[300px] lg:h-[544px] lg:w-[368px]"
-            />
-            <div className="absolute bottom-5 left-5 text-white">
-              <h1 className="font-milonga lg:text-2xl">Kebaya Gajah Mada</h1>
-              <h2 className="font-urbanist lg:text-xl">Bali, Indonesia</h2>
-            </div>
-          </div>
-        </div>
-        <div className="w-[300px] lg:w-[368px] px-2">
-          <div className="relative">
-            <img
-              src={clothCard1}
-              alt=""
-              className="object-cover h-[323px] w-[300px] lg:h-[544px] lg:w-[368px]"
-            />
-            <div className="absolute bottom-5 left-5 text-white">
-              <h1 className="font-milonga lg:text-2xl">Kebaya Gajah Mada</h1>
-              <h2 className="font-urbanist lg:text-xl">Bali, Indonesia</h2>
-            </div>
-          </div>
-        </div>
-        <div className="w-[300px] lg:w-[368px] px-2">
-          <div className="relative">
-            <img
-              src={clothCard1}
-              alt=""
-              className="object-cover h-[323px] w-[300px] lg:h-[544px] lg:w-[368px]"
-            />
-            <div className="absolute bottom-5 left-5 text-white">
-              <h1 className="font-milonga lg:text-2xl">Kebaya Gajah Mada</h1>
-              <h2 className="font-urbanist lg:text-xl">Bali, Indonesia</h2>
-            </div>
-          </div>
-        </div>
-        <div className="w-[300px] lg:w-[368px] px-2">
-          <div className="relative">
-            <img
-              src={clothCard1}
-              alt=""
-              className="object-cover h-[323px] w-[300px] lg:h-[544px] lg:w-[368px]"
-            />
-            <div className="absolute bottom-5 left-5 text-white">
-              <h1 className="font-milonga lg:text-2xl">Kebaya Gajah Mada</h1>
-              <h2 className="font-urbanist lg:text-xl">Bali, Indonesia</h2>
-            </div>
-          </div>
-        </div>
-       
-      </Slider>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
-};
+}
 
-export default ClothDetailCardSlider;
+const data = [
+  {
+    name: `John Morgan`,
+    img: `/students/John_Morgan.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    name: `Ellie Anderson`,
+    img: `/students/Ellie_Anderson.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    name: `Nia Adebayo`,
+    img: `/students/Nia_Adebayo.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    name: `Rigo Louie`,
+    img: `/students/Rigo_Louie.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    name: `Mia Williams`,
+    img: `/students/Mia_Williams.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+];
+
+export default App;
