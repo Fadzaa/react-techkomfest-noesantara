1dff612c561ea2e89cc7c0cfb8ed0e7ec60cb04e
Nareswara05 <naresalhaq9@gmail.com>
2023-12-24 23:32:09 +0700

    fix bug minor

--- src/component/MainComponent/AboutComponent/AboutComponentTeam/PersonDescription.jsx
+++ src/component/MainComponent/AboutComponent/AboutComponentTeam/PersonDescription.jsx
@@ -4,7 +4,7 @@ export default function PersonDescription({descriptionTop, descriptionBottom}) {
     return (
         <div className="w-full lg:w-2/3 lg:pe-16 flex flex-col  mt-4 h-[32vh]">
             <h1 className="font-urbanist font-bold text-lg text-primaryText mb-2">On How To Get Here</h1>
-            <div className="font-urbanist font-normal text-sm text-primaryText opacity-50">
+            <div className="font-urbanist font-normal text-sm text-primaryText opacity-50 lg:text-xl">
                 <p>{descriptionTop}</p>
                 <br/>
                 <p>{descriptionBottom}</p>
